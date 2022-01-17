bf9b0e16c27b3c4f67785bc2d282dbd09be06677
Thomas Crider <gloriouseggroll@gmail.com>
2022-01-16 19:20:50 -0700

    add staging

--- dlls/secur32/schannel.c
+++ dlls/secur32/schannel.c
@@ -915,7 +915,7 @@ static SECURITY_STATUS SEC_ENTRY schan_InitializeSecurityContextW(
         ptsExpiry->HighPart = 0;
     }
 
-    if (!phContext)
+    if (!phContext || (phNewContext && !pInput))
     {
         ULONG_PTR handle;
 
